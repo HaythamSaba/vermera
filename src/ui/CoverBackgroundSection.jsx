9de94bba320ff058141b94564c328df2e062b351
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-09-03 21:16:10 +0200

    fixing cover background section not showing image

--- src/ui/CoverBackgroundSection.jsx
+++ src/ui/CoverBackgroundSection.jsx
@@ -3,7 +3,7 @@ import Breadcrumb from "./Breadcrumb";
 const CoverBackgroundSection = ({ title, path }) => {
   return (
     <div className="relative">
-      <div className="h-[316px] w-full relative bg-[url('/images/products-background.jpg')] bg-cover bg-center bg-no-repeat before:content-[''] before:absolute before:inset-0 before:bg-cream/50 before:backdrop-blur-xs">
+      <div className="h-[316px] w-full relative bg-[url('/images/products-background.jpg')] bg-cover bg-center bg-no-repeat  before:content-['']  before:absolute before:inset-0 before:backdrop-blur-md">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center flex flex-col items-center gap-2">
           <img src="/images/main-logo.png" alt="" aria-hidden="true" />
           <h1 className="font-serif text-espresso text-5xl font-medium my-2">
